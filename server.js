439115c1410befdf12329dd3fe094b8b485a94bc
UditSax3na <uditsaxena967@gmail.com>
2025-06-12 19:59:26 +0530

    second commmit

--- server.js
+++ server.js
@@ -97,5 +97,5 @@ app.post('/addSchool', async (req, res)=>{
     });
 })
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on ${PORT}`);
 });
